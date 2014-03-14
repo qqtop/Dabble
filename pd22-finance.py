291cd22b756522c8423d5e7ffdbd0a2966c3c34d
qqtop <pict100@gmail.com>
2014-03-14 08:16:38 +0800

    Update pd22-finance.py

--- pd22-finance.py
+++ pd22-finance.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 
 
+
 startdate = datetime.date(2006,1,1)
 today = enddate = datetime.date.today()
 ticker = '0327.HK'
